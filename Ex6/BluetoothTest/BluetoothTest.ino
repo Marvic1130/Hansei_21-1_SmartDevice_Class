446c2ed29eef6ebb7bff1713344dc8caf56e0405
Marvic1130 <marvic1130@gmail.com>
2021-04-13 22:01:53 +0900

    Bluetooth Test 수정1

--- Ex6/BluetoothTest/BluetoothTest.ino
+++ Ex6/BluetoothTest/BluetoothTest.ino
@@ -1,26 +1,34 @@
-#include <BluetoothSerial.h>
+#include "BluetoothSerial.h"
 
-BluetoothSerial BT;
-int led;
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
 
-void setup(){
-    Serial.begin(115200);
-    BT.begin("ESP32Test");
-    Serial.println("The device started, now you can pair it with bluetooth!");
+BluetoothSerial SerialBT;
+
+#define LED2 27
+
+void setup() {
+  Serial.begin(115200);
+  SerialBT.begin("ESP32test");
+  Serial.println("The device started, now you can pair it with bluetooth!");
+  
+  pinMode(LED2, OUTPUT);
 }
 
-void loop(){
-    if(Serial.available())
-        BT.write(Serial.read());
-
-    if(BT.available()){
-        char txt = BT.read();
-        Serial.write(txt);
-        if(txt == 'o' || txt == 'O'){
-            digitalWrite(led, HIGH);
-        }
-        else if(txt == 'x' || txt == 'X'){
-            digitalWrite(led, LOW);
-        }
+void loop() {
+  if (Serial.available()) {
+    SerialBT.write(Serial.read());
+  }
+  
+  if (SerialBT.available()) {
+    char txt = SerialBT.read();
+    Serial.write(txt);
+    if(txt == 'o'){
+        digitalWrite(LED2, HIGH);
+    } else if(txt == 'x'){
+        digitalWrite(LED2, LOW);
     }
+  }
+  delay(20);
 }
